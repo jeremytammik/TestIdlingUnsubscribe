398b332079f04354718c9c3fdd02e7bfa253aa50
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2013-11-11 10:08:08 +0100

    added license and readme file, defined initisl version number

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ TestIdlingUnsubscribe
 =====================
 
 Revit add-in to test unsubscribing from the Idling event.
+
+For more information, please refer to The Building Coder
+
+http://thebuildingcoder.typepad.com
--- TestIdlingUnsubscribe/Properties/AssemblyInfo.cs
+++ TestIdlingUnsubscribe/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "Revit Add-In Title" )]
-[assembly: AssemblyDescription( "Revit Add-In Description" )]
+[assembly: AssemblyTitle( "TestIdlingUnsubscribe" )]
+[assembly: AssemblyDescription( "Revit add-in to test unsubscribing from the Idling event." )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "Autodesk Inc." )]
-[assembly: AssemblyProduct( "Revit Add-In Wizard Template Application" )]
+[assembly: AssemblyProduct( "TestIdlingUnsubscribe" )]
 [assembly: AssemblyCopyright( "Copyright 2013 © Jeremy Tammik Autodesk Inc." )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "2014.0.0.0" )]
+[assembly: AssemblyFileVersion( "2014.0.0.0" )]
